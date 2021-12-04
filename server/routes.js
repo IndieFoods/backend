ce40fca52378883d91d9c75f4d7cb29b6cda7d3d
kunaljain0212 <jainkunal209@gmail.com>
2021-12-04 13:04:22 +0530

    fix order router

--- server/routes.js
+++ server/routes.js
@@ -2,10 +2,12 @@ import authRouter from './api/controllers/auth/routes';
 import userRouter from './api/controllers/user/routes';
 import chefRouter from './api/controllers/chef/routes';
 import menuItemsRouter from './api/controllers/menuItem/routes';
+import orderRouter from './api/controllers/orders/routes';
 
 export default function routes(app) {
   app.use('/api/v1/auth', authRouter);
   app.use('/api/v1/user', userRouter);
   app.use('/api/v1/chef', chefRouter);
   app.use('/api/v1/menuItems', menuItemsRouter);
+  app.use('/api/v1/order', orderRouter);
 }
